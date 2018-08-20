c22492c27a60b440e94eb37bdcdfc24f3a52d95b
Jack.Wu <xuwupeng2000@gmail.com>
2018-08-20 22:41:10 +1200

    Add http model, render a list of pkgs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestapiProvider } from '../../providers/restapi/restapi';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pkgs: any
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public rest: RestapiProvider) {
+    rest.loadPackages().subscribe(
+      res => { this.pkgs = res.data; }
+    );
   }
 
 }
---
+++ src/providers/restapi/restapi.ts
@@ -0,0 +1,25 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/Rx';
+
+
+/*
+  Generated class for the RestapiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestapiProvider {
+  private baseUrl: string = "http://localhost:3000/api/v1";
+
+
+  constructor(public http: Http) {
+  }
+
+  loadPackages() {
+    return this.http.get(`${this.baseUrl}/packages`)
+      .map(res => res.json());
+  }
+
+}
